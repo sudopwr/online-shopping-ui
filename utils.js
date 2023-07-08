7e1930acdde8f33dc9430f4d8ed8602bcaa6fd94
sudopwr <sudopwr@gmail.com>
2023-07-08 17:46:11 +0530

    feat: handle menu after login

--- utils.js
+++ utils.js
@@ -1,17 +1,41 @@
-const USER_TOKEN_KEY = "USER_TOKEN_KEY"
+const USER_TOKEN_KEY = 'USER_TOKEN_KEY'
+const LOGIN_NAV_ITEM_KEY = 'login-nav-item'
+const PRODUCTS_NAV_ITEM_KEY = 'products-nav-item'
+const LOGOUT_NAV_ITEM_KEY = 'logout-nav-item'
 
 const setValueInLocalStorage = (key, value) => {
     localStorage.setItem(key, value)
 }
 
-const getValueInLocalStorage = (key) => {
-    localStorage.getItem(key)
+const getValueFromLocalStorage = (key) => {
+    return localStorage.getItem(key)
 }
 
 const clearValueInLocalStorage = (key) => {
-    localStorage.setItem(key, '')
+    localStorage.removeItem(key)
 }
 
-const hideNavItems = () => {
-    document.getElementById("login-nav-item").style.display = 'none'
+const setDisplayProperty = (id, value) => {
+    if (document.getElementById(id)) {
+        document.getElementById(id).style.display = value
+    }
 }
+
+const onPageLoadNavSettings = () => {
+    if (getValueFromLocalStorage(USER_TOKEN_KEY)) {
+        setDisplayProperty(LOGIN_NAV_ITEM_KEY, 'none')
+        setDisplayProperty(PRODUCTS_NAV_ITEM_KEY, 'block')
+        setDisplayProperty(LOGOUT_NAV_ITEM_KEY, 'block')
+    } else {
+        setDisplayProperty(LOGIN_NAV_ITEM_KEY, 'block')
+        setDisplayProperty(PRODUCTS_NAV_ITEM_KEY, 'none')
+        setDisplayProperty(LOGOUT_NAV_ITEM_KEY, 'none')
+    }
+}
+
+const getLogout = () => {
+    clearValueInLocalStorage(USER_TOKEN_KEY)
+    window.location = "/"
+}
+
+onPageLoadNavSettings()
